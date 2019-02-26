f8bead2feb2c4fec75ccc03d64d03e41c3ead41a
zecollokaris <collo.kariss@gmail.com>
2019-02-26 10:38:18 +0300

    added image logo

--- app/src/main/java/com/splashscreen/splashscreen/SplashScreen.java
+++ app/src/main/java/com/splashscreen/splashscreen/SplashScreen.java
@@ -27,6 +27,7 @@ public class SplashScreen extends AppCompatActivity {
 //      We reference the image and text using their id.
 //      descimage = (R.id.titleimage)
 //      desctxt = (R.id.titletxt)
+
         descimage = (LinearLayout) findViewById(R.id.titleimage);
         desctxt = (LinearLayout) findViewById(R.id.titletxt);
         uptodown = AnimationUtils.loadAnimation(this,R.anim.uptodown);
